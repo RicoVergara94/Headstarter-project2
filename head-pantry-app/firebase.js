c99116e901ff678ea1153034726db67c656afa84
Oscar Vergara <oscar.vergara1994@gmail.com>
2024-07-31 15:06:59 -0400

    set up firebase and removed unnessessary cs

---
+++ head-pantry-app/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiCCzqVdhZV9cpbu1_8D873AZt0bHBJ9M",
+  authDomain: "inventory-management-f3305.firebaseapp.com",
+  projectId: "inventory-management-f3305",
+  storageBucket: "inventory-management-f3305.appspot.com",
+  messagingSenderId: "908780229584",
+  appId: "1:908780229584:web:13430e6eba0c21db2d5fd5",
+  measurementId: "G-R2C015S4NF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const fireStore = getFirestore(app);
+
+export { fireStore };
