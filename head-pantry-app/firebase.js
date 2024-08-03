cdd894beac54bb14bf52bc6efd907c68f7d2b5ce
Oscar Vergara <oscar.vergara1994@gmail.com>
2024-08-03 01:20:58 -0400

    added inventory list to interface

--- head-pantry-app/firebase.js
+++ head-pantry-app/firebase.js
@@ -20,6 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const fireStore = getFirestore(app);
+const db = getFirestore(app);
 
-export { fireStore };
+export { db };
