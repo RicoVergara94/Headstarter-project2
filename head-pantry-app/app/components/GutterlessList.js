5738b94166fda307b7a599ae8d639c718f17ed1a
Oscar Vergara <oscar.vergara1994@gmail.com>
2024-08-03 02:51:38 -0400

    added addItem and removeItem functionality

---
+++ head-pantry-app/app/components/GutterlessList.js
@@ -0,0 +1,54 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import CommentIcon from "@mui/icons-material/Comment";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+
+const GutterlessList = (props) => {
+  return (
+    <List
+      sx={{
+        width: "100%",
+        maxWidth: 360,
+        bgcolor: "background.paper",
+        position: "relative",
+        top: 10,
+        left: 50,
+        textAlign: "center",
+      }}
+    >
+      {props.inventory.map((item) => (
+        <ListItem
+          sx={{
+            textAlign: "left",
+          }}
+          key={item.name}
+          disableGutters
+          secondaryAction={
+            <div style={{ left: 10 }}>
+              <Button
+                className="Button"
+                variant="contained"
+                onClick={() => props.addItem(item)}
+              >
+                Add Item
+              </Button>
+              <Button
+                className="Button"
+                variant="contained"
+                onClick={() => props.removeItem(item)}
+              >
+                Remove Item
+              </Button>
+            </div>
+          }
+        >
+          <ListItemText primary={`${item.name}`} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GutterlessList;
--- head-pantry-app/app/page.js
+++ head-pantry-app/app/page.js
@@ -4,12 +4,15 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { Box, Typography } from "@mui/material";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import CommentIcon from "@mui/icons-material/Comment";
-import IconButton from "@mui/material/IconButton";
-import { query, collection, getDocs } from "firebase/firestore";
+import GutterlessList from "./components/GutterlessList";
+import {
+  query,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
@@ -34,45 +37,44 @@ export default function Home() {
     updateInventory();
   }, []);
 
-  const GutterlessList = () => {
-    return (
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "background.paper",
-        }}
-      >
-        {inventory.map((item) => (
-          <ListItem
-            sx={{
-              textAlign: "center",
-            }}
-            key={item}
-            disableGutters
-            secondaryAction={
-              <IconButton aria-label="comment">
-                <CommentIcon />
-              </IconButton>
-            }
-          >
-            <ListItemText primary={`${item.name}`} />
-          </ListItem>
-        ))}
-      </List>
-    );
+  const removeItem = async (item) => {
+    const docRef = doc(db, `inventory/${item.name}`);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity >= 1) {
+        setDoc(docRef, { quantity: quantity - 1 });
+      }
+    } else {
+      console.log("do nothing");
+    }
   };
+
+  const addItem = async (item) => {
+    const docRef = doc(db, `inventory/${item.name}`);
+    const docSnap = await getDoc(docRef);
+    const { quantity } = docSnap.data();
+    await setDoc(docRef, { quantity: quantity + 1 });
+  };
+
   return (
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        position: "relative",
+        display: "grid",
+        placeitems: "center",
       }}
     >
       <Typography variant="h1">Inventory Management</Typography>
       <div>
-        <GutterlessList />
+        <GutterlessList
+          inventory={inventory}
+          removeItem={removeItem}
+          addItem={addItem}
+        />
       </div>
     </Box>
   );
